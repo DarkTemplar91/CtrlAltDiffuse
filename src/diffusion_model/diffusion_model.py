917f2db4001e35f4a203e6a8993b00c3589fe9aa
Erik <szaszco@gmail.com>
2024-11-02 21:18:39 +0100

    misc

--- src/diffusion_model/diffusion_model.py
+++ src/diffusion_model/diffusion_model.py
@@ -1,6 +1,7 @@
 import pytorch_lightning as pl
 import torch
 from torchvision import transforms
+import torch.nn.functional as F
 from torchmetrics.image import FrechetInceptionDistance, InceptionScore, KernelInceptionDistance
 
 
@@ -16,9 +17,32 @@ class DiffusionModel(pl.LightningModule):
         self.is_metric = InceptionScore(normalize=True)
         self.kid_metric = KernelInceptionDistance(subset_size=50, normalize=True)
 
+    def sample_images(self, num_samples):
+        # Sample images from the diffusion model
+        # TODO: Implement model
+        return self.model.sample(num_samples)
+
     def training_step(self, batch, batch_idx):
+        generated_images = self.sample_images(self.batch_size)
+        mse_loss = F.mse_loss(generated_images, batch)
+        self.log('train_mse_loss', mse_loss)
+        self.log('train_rmse_loss', torch.sqrt(mse_loss))
         raise NotImplementedError
 
+    def validation_step(self, batch, batch_idx):
+        generated_images = self.sample_images(self.batch_size)
+        mse_loss = F.mse_loss(generated_images, batch)
+        self.log('train_mse_loss', mse_loss)
+        self.log('train_rmse_loss', torch.sqrt(mse_loss))
+        raise NotImplementedError
+
+    def test_step(self, batch, batch_idx):
+        raise NotImplementedError
+
+    def configure_optimizers(self):
+        raise NotImplementedError
+
+    # Metrics for testing
     def calculate_metrics(self, generated_images):
         self.fid_metric.update(generated_images, real=False)
         fid_score = self.fid_metric.compute()
@@ -30,17 +54,3 @@ class DiffusionModel(pl.LightningModule):
         kid_score = self.kid_metric.compute()
 
         return fid_score, is_score, kid_score
-
-    def sample_images(self, num_samples):
-        # Sample images from the diffusion model
-        # TODO: Implement model
-        return self.model.sample(num_samples)
-
-    def validation_step(self, batch, batch_idx):
-        raise NotImplementedError
-
-    def test_step(self, batch, batch_idx):
-        raise NotImplementedError
-
-    def configure_optimizers(self):
-        raise NotImplementedError
